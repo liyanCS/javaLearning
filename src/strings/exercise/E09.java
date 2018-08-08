8cf85f9414755a9695363b57a3d3be9890da7d3f
liyanCS <li_yanliyan@163.com>
2018-08-08 16:46:24 +0800

    the second commit files

---
+++ src/strings/exercise/E09.java
@@ -0,0 +1,11 @@
+package strings.exercise;
+
+/**
+ * @author Li Yan
+ */
+public class E09 {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+        System.out.println("Retry....");
+    }
+}
