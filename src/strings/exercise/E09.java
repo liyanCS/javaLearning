8b210a27e79a23d05bd35bb7bb90125d9513be61
liyanCS <li_yanliyan@163.com>
2018-08-08 17:14:35 +0800

    提交1次看看

--- src/strings/exercise/E09.java
+++ src/strings/exercise/E09.java
@@ -7,5 +7,6 @@ public class E09 {
     public static void main(String[] args) {
         System.out.println("hello, world!");
         System.out.println("Retry....");
+        System.out.println("Third....");
     }
 }
