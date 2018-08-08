85e0996722c59bc1d3946392529d8dd60a8814cf
liyanCS <li_yanliyan@163.com>
2018-08-08 17:48:50 +0800

    one commit

--- src/strings/exercise/E08.java
+++ src/strings/exercise/E08.java
@@ -4,4 +4,9 @@ package strings.exercise;
  * @author Li Yan
  */
 public class E08 {
+    public static void main(String[] args) {
+        System.out.println("one");
+        System.out.println("two");
+        System.out.println("three");
+    }
 }
